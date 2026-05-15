f0f5c533e47949f664c9262048619975e7765838
hiepndn <hninenp@gmail.com>
2026-05-15 20:07:36 +0700

    test fix bug trên androi

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AppProviders } from './app/providers';
+import { registerSW } from 'virtual:pwa-register';
+
+// Cố tình register Service Worker thay vì đợi Vite tự inject
+registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
